33176843702862001ef7480963c119b570a97f47
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2022-03-07 13:42:52 +0100

    bugs check

--- api/login.php
+++ api/login.php
@@ -9,35 +9,39 @@ session_start();
 $username = $_REQUEST['username'];
 $password = $_REQUEST['password'];
 //die(json_encode(['message'=>"Access Deny"])); 
-if(empty($username) || empty($password)){
-  $msg = "جميع الحقول مطلوبة";
-  $code = 300;
-}else{
-  require_once("../php/dbconnection.php");
-  $sql = "select * from clients where phone = ?";
-  $result = getData($con,$sql,[$username]);
-  if(count($result) != 1 || !password_verify($password,$result[0]['password']) ){
-    $msg = "اسم المستخدم او كلمة المرور غير صحيحة";
+try {
+  if (empty($username) || empty($password)) {
+    $msg = "جميع الحقول مطلوبة";
     $code = 300;
-  }else{
-    $msg = 1;
-    $code = 200;
-    $data['login']=1;
-    $data['phone']=$result[0]['phone'];
-    $data['userid']=$result[0]['id'];
-    $data['name']=$result[0]['name'];
-    $token=$result[0]['api_token'];
-    if($result[0]['api_token'] == "" || $result[0]['api_token'] == 0 || empty($result[0]['api_token']) || $result[0]['api_token'] = null){
-        $token = uniqid().uniqid().uniqid().uniqid();
+  } else {
+    require_once("../php/dbconnection.php");
+    $sql = "select * from clients where phone = ?";
+    $result = getData($con, $sql, [$username]);
+    if (count($result) != 1 || !password_verify($password, $result[0]['password'])) {
+      $msg = "اسم المستخدم او كلمة المرور غير صحيحة";
+      $code = 300;
+    } else {
+      $msg = 1;
+      $code = 200;
+      $data['login'] = 1;
+      $data['phone'] = $result[0]['phone'];
+      $data['userid'] = $result[0]['id'];
+      $data['name'] = $result[0]['name'];
+      $token = $result[0]['api_token'];
+      if ($result[0]['api_token'] == "" || $result[0]['api_token'] == 0 || empty($result[0]['api_token']) || $result[0]['api_token'] = null) {
+        $token = uniqid() . uniqid() . uniqid() . uniqid();
         $sql = "update clients set api_token = ? where id=?";
-        $res1 = setData($con,$sql,[$token,$result[0]['id']]);
-        if(!$res1){
+        $res1 = setData($con, $sql, [$token, $result[0]['id']]);
+        if (!$res1) {
           $token = "";
         }
+      }
     }
-
   }
+} catch (PDOException $ex) {
+  $data = ["error" => $ex];
+  $success = "0";
+  $msg = "Query Error";
 }
-ob_end_clean();
-echo json_encode(['data'=>$data,'token'=>$token,'code'=>$code,'message'=>$msg]);
-?>
+//ob_end_clean();
+echo json_encode(['data' => $data, 'token' => $token, 'code' => $code, 'message' => $msg]);
