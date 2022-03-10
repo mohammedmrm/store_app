a4c2ee6e8afb3301d1aeba8f265b555d1da4d6dc
mohammedmrm <60153318+mohammedmrm@users.noreply.github.com>
2022-03-10 19:02:47 +0100

    bug

--- index.php
+++ index.php
@@ -279,7 +279,7 @@ include_once("config.php");
   <script src="styles/select2-4.0.13/dist/js/select2.min.js"></script>
   <script type="text/javascript">
     if ('serviceWorker' in navigator) {
-      nuavigator.serviceWorker.register('/sw.js');
+      nuavigator.serviceWorker.register('sw.js');
     }
 
     function getCities(elem) {
